616d0b6db6f5a44b9d5c288f7805a380d32f40c0
irmasaki <46283479+irmasaki@users.noreply.github.com>
2020-01-25 16:36:41 +0900

    add Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,8 +25,21 @@ function App() {
         >
           Learn React
         </a>
+        <Link to="/test"><p>test</p></Link>
+        <Link to="/irisawa"><p>irisawa</p></Link>
+        <Link to="/yamazaki"><p>yamazaki</p></Link>
+        <Link to="/test2"><p>test2</p></Link>
+        <Link to="/test3"><p>test3</p></Link>
       </header>
+          <Switch>
+            <Route path="/test"><p>test</p></Route>
+            <Route path="/irisawa"><p>irisawa</p></Route>
+            <Route path="/yamazaki"><p>yamazaki</p></Route>
+            <Route path="/test2"><p>test2</p></Route>
+            <Route path="/test3"><p>test3</p></Route>
+          </Switch>
     </div>
+    </Router>
   );
 }
 
